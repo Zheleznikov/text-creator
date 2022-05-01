d763f3bd3916e86b9bb17d3cf870804afcdfe145
Serg Zhe <polwen@yandex.ru>
2022-05-01 10:03:41 +0300

    add text-creator

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import markovify
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def create_random_text(number_of_sentences):
+    text = open('example.txt', encoding='utf8').read()
+    text_model = markovify.Text(text)
+    for i in range(number_of_sentences):
+        print(text_model.make_sentence())
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # как можно сделать
+    # 1. выделить в шаблоне места, в которых будет КИ
+    # 2. написать функцию, которая будет подставлять соответствующее случайное КИ в нужное место
+    # 3. создать новый текст с вымышленным КИ в шаблоне
+    # 4. Создать случайный текст с помощью цепей Маркова
+    create_random_text(10)
+    # 5. Записать это текст в нужный формат
